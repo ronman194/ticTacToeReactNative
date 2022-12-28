1df96406660aa83840078e3541f68ca0882c51d6
ronman194 <mansharofron@gmail.com>
2022-12-28 19:38:00 +0200

    finish tic tac toe

--- App.tsx
+++ App.tsx
@@ -1,305 +1,189 @@
-// import { FC, useState } from 'react';
-// import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar } from 'react-native';
-
-// const Brick: FC<{ callback: (ind: number, r: number, c: number) => void, index: number, row: number, col: number, turn: { player: number } }> = ({ callback, index, turn, row, col }) => {
-//   const [player, setPlayer] = useState<number>(0);
-//   const colors = ["white", "green", "black"];
-//   const [board, setBoard] = useState<number[]>([]);
-
-//   const clickHandler = () => {
-//     console.log("CLICK BRICK");
-//     if (board.includes(index)) {
-//       return;
-//     }
-//     setBoard(prevIndex => [...prevIndex, index]);
-//     console.log(board);
-//     setPlayer(turn.player);
-//     callback(index, row, col);
-//   }
-//   return (
-//     <View style={styles.brick}>
-//       <TouchableOpacity disabled={board.includes(index)} style={[styles.brick, { backgroundColor: colors[player] }]} onPress={clickHandler}>
-//       </TouchableOpacity>
-//     </View>
-//   )
-// }
-
-// const App: FC = () => {
-//   const turn = { player: 1 };
-//   const moves = { p: [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]] }
-
-//   const checkWin = (player: number) => {
-//     //check rows
-//     for (let i = 0; i < moves.p.length; i++) {
-//       if (moves.p[i][0] !== -1 && moves.p[i][0] === moves.p[i][1] && moves.p[i][1] === moves.p[i][2]) {
-//         alert(`Player ${player} wins!`);
-//         return;
-//       }
-//     }
-//     //check cols
-//     for (let i = 0; i < moves.p.length; i++) {
-//       if (moves.p[0][i] !== -1 && moves.p[0][i] === moves.p[1][i] && moves.p[1][i] === moves.p[2][i]) {
-//         alert(`Player ${player} wins!`);
-//         return;
-//       }
-//     }
-//     //check diagonals
-//     if (moves.p[0][0] !== -1 && moves.p[0][0] === moves.p[1][1] && moves.p[1][1] === moves.p[2][2]) {
-//       alert(`Player ${player} wins!`);
-//       return;
-//     }
-//     if (moves.p[0][2] !== -1 && moves.p[0][2] === moves.p[1][1] && moves.p[1][1] === moves.p[2][0]) {
-//       alert(`Player ${player} wins!`);
-//       return;
-//     }
-
-//     let fillCells = 0;
-//     for (let i = 0; i < moves.p.length; i++) {
-//       for (let j = 0; j < moves.p.length; j++) {
-//         if (moves.p[i][j] !== -1) {
-//           fillCells += 1;
-//         }
-//       }
-//     }
-//     if (fillCells === 9) {
-//       alert("Its a draw!!!");
-//     }
-//   }
-
-//   const resetHandler = () => {
-//     for (let i = 0; i < moves.p.length; i++) {
-//       for (let j = 0; j < moves.p.length; j++) {
-//         if (moves.p[i][j] !== -1) {
-//           moves.p[i][j] = -1;
-//         }
-//       }
-//     }
-//   }
-
-//   const clickHandler = (index: number, row: number, col: number) => {
-//     console.log("CLICK APP " + index);
-//     if (turn.player == 1) {
-//       moves.p[row][col] = turn.player;
-//       console.log("MOVES " + moves.p);
-//       checkWin(turn.player);
-//       turn.player = 2;
-//     }
-//     else {
-//       moves.p[row][col] = turn.player;
-//       console.log("MOVES " + moves.p);
-//       checkWin(turn.player);
-//       turn.player = 1;
-//     }
-//   }
-//   return (
-//     <View style={styles.container}>
-
-//       <View style={styles.rowContainer}>
-//         <Brick callback={clickHandler} index={0} turn={turn} row={0} col={0}></Brick>
-//         <Brick callback={clickHandler} index={1} turn={turn} row={0} col={1}></Brick>
-//         <Brick callback={clickHandler} index={2} turn={turn} row={0} col={2}></Brick>
-//       </View>
-
-//       <View style={styles.rowContainer}>
-//         <Brick callback={clickHandler} index={3} turn={turn} row={1} col={0}></Brick>
-//         <Brick callback={clickHandler} index={4} turn={turn} row={1} col={1}></Brick>
-//         <Brick callback={clickHandler} index={5} turn={turn} row={1} col={2}></Brick>
-//       </View>
-
-//       <View style={styles.rowContainer}>
-//         <Brick callback={clickHandler} index={6} turn={turn} row={2} col={0}></Brick>
-//         <Brick callback={clickHandler} index={7} turn={turn} row={2} col={1}></Brick>
-//         <Brick callback={clickHandler} index={8} turn={turn} row={2} col={2}></Brick>
-//       </View>
-
-//       <View style={styles.button}>
-//         <TouchableOpacity onPress={resetHandler}>
-//           <Text style={styles.buttonText}>
-//             Restart
-//           </Text>
-//         </TouchableOpacity>
-//       </View>
-
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     marginTop: StatusBar.currentHeight,
-//     flex: 1,
-//     backgroundColor: 'yellow',
-//   },
-//   rowContainer: {
-//     flexDirection: 'row',
-//   },
-//   brick: {
-//     flex: 1,
-//     margin: 10,
-//     aspectRatio: 1,
-//     // backgroundColor: 'blue',
-//   },
-//   button: {
-//     alignSelf: "center",
-//     margin: 20,
-//     justifyContent: "center",
-//     backgroundColor: "#DDDDDD",
-//     borderRadius: 20,
-//     height: 50,
-//     width: 200,
-//   },
-//   buttonText: {
-//     fontSize: 30,
-//     textAlign: 'center',
-//   }
-// });
-// export default App;
-
 import { FC, useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar, SafeAreaView } from 'react-native';
 
-type Player = {
-  player: number
-}
-
-type Props = {
-  index: number,
-  state: number,
-  turn: Player,
-  onClick: (index: number, row: number, col: number) => void,
+type BrickProps = {
   row: number,
   col: number,
+  value: number,
+  onPress: () => void;
 }
 
-const Brick: FC<{ props: Props }> = ({ props }) => {
-  const colors = ["white", "green", "black"];
-  const [board, setBoard] = useState<number[]>([]);
+const Brick: FC<{ props: BrickProps }> = ({ props }) => {
 
+  const images = [
+    require("./assets/touchScreen.png"),
+    require("./assets/x.png"),
+    require("./assets/o.png")
+  ]
 
-  const clickHandler = () => {
-    if (board.includes(props.index)) {
-      console.log(board);
-      return;
-    }
-    setBoard(prevIndex => [...prevIndex, props.index]);
-    props.state = props.turn.player;
-    console.log("turn: " + props.turn.player);
-    console.log("state: " + props.state);
-    props.onClick(props.index, props.row, props.col);
-  }
   return (
-    <View style={[styles.brick, { backgroundColor: colors[props.state] }]}>
-      <TouchableOpacity disabled={props.state !== 0} style={[styles.brick, { backgroundColor: colors[props.state] }]} onPress={clickHandler}>
-      </TouchableOpacity>
+    <View style={[styles.brick, { backgroundColor: "#2f3e46" }]}>
+      <Image resizeMode="contain" source={images[props.value]} style={styles.brickImage}></Image>
     </View>
   )
 }
-
 const App: FC = () => {
-  const turn: Player = { player: 1 };
-  const moves = { p: [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]] }
+
+  const colors = ["#7fc8f8", "#ff6392"];
+
+  const [board, setBoard] = useState([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [gameOver, setGameOver] = useState(false);
+  const [playerXScore, setPlayerXScore] = useState(0);
+  const [playerOScore, setPlayerOScore] = useState(0);
 
   const checkWin = (player: number) => {
     //check rows
-    for (let i = 0; i < moves.p.length; i++) {
-      if (moves.p[i][0] !== -1 && moves.p[i][0] === moves.p[i][1] && moves.p[i][1] === moves.p[i][2]) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        if (player == 1) {
+          setPlayerXScore(playerXScore + 1);
+        } else {
+          setPlayerOScore(playerOScore + 1);
+        }
+        setGameOver(true);
         alert(`Player ${player} wins!`);
         return;
       }
     }
     //check cols
-    for (let i = 0; i < moves.p.length; i++) {
-      if (moves.p[0][i] !== -1 && moves.p[0][i] === moves.p[1][i] && moves.p[1][i] === moves.p[2][i]) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[0][i] !== 0 && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        if (player == 1) {
+          setPlayerXScore(playerXScore + 1);
+        } else {
+          setPlayerOScore(playerOScore + 1);
+        }
+        setGameOver(true);
         alert(`Player ${player} wins!`);
         return;
       }
     }
     //check diagonals
-    if (moves.p[0][0] !== -1 && moves.p[0][0] === moves.p[1][1] && moves.p[1][1] === moves.p[2][2]) {
+    if (board[0][0] !== 0 && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      if (player == 1) {
+        setPlayerXScore(playerXScore + 1);
+      } else {
+        setPlayerOScore(playerOScore + 1);
+      }
+      setGameOver(true);
       alert(`Player ${player} wins!`);
       return;
     }
-    if (moves.p[0][2] !== -1 && moves.p[0][2] === moves.p[1][1] && moves.p[1][1] === moves.p[2][0]) {
+    if (board[0][2] !== 0 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      if (player == 1) {
+        setPlayerXScore(playerXScore + 1);
+      } else {
+        setPlayerOScore(playerOScore + 1);
+      }
+      setGameOver(true);
       alert(`Player ${player} wins!`);
       return;
     }
 
     let fillCells = 0;
-    for (let i = 0; i < moves.p.length; i++) {
-      for (let j = 0; j < moves.p.length; j++) {
-        if (moves.p[i][j] !== -1) {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        if (board[i][j] !== 0) {
           fillCells += 1;
         }
       }
     }
     if (fillCells === 9) {
+      setGameOver(true);
       alert("Its a draw!!!");
     }
   }
 
+  const handlePress = (row: number, col: number) => {
+    if (board[row][col] !== 0 || gameOver) {
+      // The brick has already been played, do nothing
+      return;
+    }
+    const newBoard = [...board];
+    newBoard[row][col] = currentPlayer;
+    setBoard(newBoard);
+    console.log("BOARD: " + board);
+    console.log("TURN: " + currentPlayer);
+    checkWin(currentPlayer);
+    setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+  };
+
   const resetHandler = () => {
-    for (let i = 0; i < moves.p.length; i++) {
-      for (let j = 0; j < moves.p.length; j++) {
-        if (moves.p[i][j] !== -1) {
-          moves.p[i][j] = -1;
-        }
-      }
-    }
+    setBoard(
+      [[0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]]);
+    setGameOver(false);
+    setCurrentPlayer(1);
   }
 
-  const clickHandler = (index: number, row: number, col: number) => {
-    console.log("CLICK APP " + index);
-    if (turn.player == 1) {
-      moves.p[row][col] = turn.player;
-      console.log("MOVES " + moves.p);
-      checkWin(turn.player);
-      turn.player = 2;
-    }
-    else {
-      moves.p[row][col] = turn.player;
-      console.log("MOVES " + moves.p);
-      checkWin(turn.player);
-      turn.player = 1;
-    }
+  const resetScoreHandler = () => {
+    resetHandler();
+    setPlayerOScore(0);
+    setPlayerXScore(0);
   }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
 
-      <View style={styles.rowContainer}>
-        <Brick props={{ index: 0, state: 0, turn: turn, onClick: clickHandler, row: 0, col: 0 }}></Brick>
-        <Brick props={{ index: 1, state: 0, turn: turn, onClick: clickHandler, row: 0, col: 1 }}></Brick>
-        <Brick props={{ index: 2, state: 0, turn: turn, onClick: clickHandler, row: 0, col: 2 }}></Brick>
+        <View>
+          <Text style={styles.header}>TIC TAC TOE</Text>
+        </View>
+
+        <View style={styles.rowContainer}>
+          <Text style={styles.playerXScore}> Player X Score: {playerXScore}</Text>
+          <Text style={styles.playerOScore}> Player O Score: {playerOScore}</Text>
+        </View>
+
+        {board.map((row, rowIndex) => (
+          <View key={rowIndex} style={styles.rowContainer}>
+            {row.map((value, colIndex) => (
+              <TouchableOpacity disabled={board[rowIndex][colIndex] != 0 || gameOver} style={styles.brick} key={colIndex} onPress={() => handlePress(rowIndex, colIndex)}>
+                <Brick
+                  key={colIndex}
+                  props={{ row: rowIndex, col: colIndex, value: board[rowIndex][colIndex], onPress: () => handlePress(rowIndex, colIndex) }}
+                />
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
+
+        <View>
+          <Text style={[styles.playerTurn, { color: currentPlayer === 1 ? colors[0] : colors[1] }]}>Player {currentPlayer === 1 ? "X" : "O"} Turn</Text>
+        </View>
+
+        <View style={styles.rowContainer}>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={resetHandler}>
+              <Text style={styles.buttonText}>
+                Play Again
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={resetScoreHandler}>
+              <Text style={styles.buttonText}>
+                Reset Score
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
       </View>
-
-      <View style={styles.rowContainer}>
-        <Brick props={{ index: 3, state: 0, turn: turn, onClick: clickHandler, row: 1, col: 0 }}></Brick>
-        <Brick props={{ index: 4, state: 0, turn: turn, onClick: clickHandler, row: 1, col: 1 }}></Brick>
-        <Brick props={{ index: 5, state: 0, turn: turn, onClick: clickHandler, row: 1, col: 2 }}></Brick>
-      </View>
-
-      <View style={styles.rowContainer}>
-        <Brick props={{ index: 6, state: 0, turn: turn, onClick: clickHandler, row: 2, col: 0 }}></Brick>
-        <Brick props={{ index: 7, state: 0, turn: turn, onClick: clickHandler, row: 2, col: 1 }}></Brick>
-        <Brick props={{ index: 8, state: 0, turn: turn, onClick: clickHandler, row: 2, col: 2 }}></Brick>
-      </View>
-
-      <View style={styles.button}>
-        <TouchableOpacity onPress={resetHandler}>
-          <Text style={styles.buttonText}>
-            Restart
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-    </View>
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     marginTop: StatusBar.currentHeight,
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#181f1c',
   },
   rowContainer: {
     flexDirection: 'row',
@@ -314,14 +198,57 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     margin: 20,
     justifyContent: "center",
-    backgroundColor: "#DDDDDD",
+    backgroundColor: '#007bff',
     borderRadius: 20,
-    height: 50,
-    width: 200,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    color: '#fff',
+    flex: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2
   },
   buttonText: {
-    fontSize: 30,
     textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase'
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#5aa9e6',
+    textAlign: 'center',
+    margin: 20
+  },
+  brickImage: {
+    width: "100%",
+    height: "100%"
+  },
+  playerXScore: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#7fc8f8',
+    textAlign: 'center',
+    margin: 20,
+    flex: 0.5,
+  },
+  playerOScore: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ff6392',
+    textAlign: 'center',
+    margin: 20,
+    flex: 0.5,
+  },
+  playerTurn: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 20,
   }
 });
 export default App;
+
+
